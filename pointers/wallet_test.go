e921f81fee8a67f7ef771ff91e34aeae45ad33ad
Alexis-Maurer Fortin <amfortin@gosecure.ca>
2020-01-18 20:48:59 -0500

    withdraw method

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -1,21 +1,34 @@
 package pointers
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance()
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
 
-	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
+		got := wallet.Balance()
 
-	want := Bitcoin(10)
+		want := Bitcoin(10)
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
