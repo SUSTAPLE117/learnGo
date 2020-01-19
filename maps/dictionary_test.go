445ba49f6ff5e82a599e460787bfa845447b2e4a
Alexis-Maurer Fortin <amfortin@gosecure.ca>
2020-01-18 23:08:47 -0500

    extracted error not found

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -14,13 +14,13 @@ func TestSearch(t *testing.T) {
 
 	t.Run("unknown word", func(t *testing.T) {
 		_, err := dictionary.Search("unknown")
-		want := "could not find the word you were looking for"
+		want := ErrNotFound
 
 		if err == nil {
 			t.Fatal("expected to get an error.")
 		}
 
-		assertStrings(t, err.Error(), want)
+		assertError(t, err, want)
 	})
 }
 
@@ -29,5 +29,17 @@ func assertStrings(t *testing.T, got, want string) {
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
+
+	if got == nil {
+		t.Fatal("expected to get an error.")
+	}
+
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
 	}
 }
