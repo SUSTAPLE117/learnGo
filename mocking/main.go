cbf110f08bfd324c566514155f1526ea80d2a0f4
Alexis-Maurer Fortin <amfortin@gosecure.ca>
2020-01-21 21:03:56 -0500

    Sleeper interface

--- mocking/main.go
+++ mocking/main.go
@@ -10,16 +10,27 @@ import (
 const countdownStart = 3
 const finalWord = "Go!"
 
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 
 	for i := countdownStart; i > 0; i-- {
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
-	time.Sleep(1 * time.Second)
+	sleeper.Sleep()
 	fmt.Fprint(out, finalWord)
 }
 
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
