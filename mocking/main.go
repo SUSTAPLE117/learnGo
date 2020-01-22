64e0787914cae18a06a3e174fa62f7d7fdd272ce
Alexis-Maurer Fortin <amfortin@gosecure.ca>
2020-01-21 20:55:11 -0500

    added timeout

--- mocking/main.go
+++ mocking/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 const countdownStart = 3
@@ -12,8 +13,10 @@ const finalWord = "Go!"
 func Countdown(out io.Writer) {
 
 	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
 		fmt.Fprintln(out, i)
 	}
+	time.Sleep(1 * time.Second)
 	fmt.Fprint(out, finalWord)
 }
 
