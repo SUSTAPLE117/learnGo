9e04d3f1cf7a90e26cb0e694798a2e41206381a9
Alexis-Maurer Fortin <amfortin@gosecure.ca>
2020-01-31 22:28:26 -0500

    timeout after 10 seconds

--- select/racer_test.go
+++ select/racer_test.go
@@ -18,11 +18,25 @@ func TestRacer(t *testing.T) {
 	fastURL := "http://www.quii.co.uk"
 
 	want := fastURL
-	got := Racer(slowURL, fastURL)
+	got, _ := Racer(slowURL, fastURL)
 
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
+
+	t.Run("return an error if the server doesn't respond within 10s", func(t *testing.T) {
+		serverA := makeDelayedServer(11 * time.Second)
+		serverB := makeDelayedServer(12 * time.Second)
+
+		defer serverA.Close()
+		defer serverB.Close()
+
+		_, err := Racer(serverA.URL, serverB.URL)
+
+		if err == nil {
+			t.Errorf("expected an error but didn't get one")
+		}
+	})
 
 }
 
