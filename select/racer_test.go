13cbc4db4aedc3a911eeba6c9b6f4aacf68b2559
Alexis-Maurer Fortin <amfortin@gosecure.ca>
2020-01-31 22:02:53 -0500

    refactoring

--- select/racer_test.go
+++ select/racer_test.go
@@ -8,14 +8,9 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(20 * time.Millisecond)
-		w.WriteHeader(http.StatusOK)
-	}))
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(0 * time.Millisecond)
 
-	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	}))
 	slowURL := "http://www.facebook.com"
 	fastURL := "http://www.quii.co.uk"
 
@@ -27,4 +22,11 @@ func TestRacer(t *testing.T) {
 	}
 	slowServer.Close()
 	fastServer.Close()
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
 }
