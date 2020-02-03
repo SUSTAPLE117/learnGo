66669617b5eff968eb9e0036037a941257427c64
Alexis-Maurer Fortin <amfortin@gosecure.ca>
2020-02-02 21:36:28 -0500

    extracted getValue

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -3,7 +3,10 @@ package reflection
 import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
-	val := reflect.ValueOf(x)
+	val := getValue(x)
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
 		switch field.Kind() {
@@ -13,4 +16,14 @@ func walk(x interface{}, fn func(input string)) {
 			walk(field.Interface(), fn)
 		}
 	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	return val
 }
