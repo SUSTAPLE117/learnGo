a778f867eeccf7470b25451663e9c7927dbc714d
Alexis-Maurer Fortin <amfortin@gosecure.ca>
2020-02-02 21:46:27 -0500

    handling arrays

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -60,6 +60,22 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"Chris", "London"},
 		},
+		{
+			"Slices",
+			[]Profile{
+				{33, "London"},
+				{34, "Reykjavík"},
+			},
+			[]string{"London", "Reykjavík"},
+		},
+		{
+			"Arrays",
+			[2]Profile{
+				{33, "London"},
+				{34, "Reykjavík"},
+			},
+			[]string{"London", "Reykjavík"},
+		},
 	}
 
 	for _, test := range cases {
