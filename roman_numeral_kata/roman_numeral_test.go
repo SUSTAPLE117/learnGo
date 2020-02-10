73f7444d8921eea4df156518ecba6f10cd947885
Alexis-Maurer Fortin <amfortin@gosecure.ca>
2020-02-09 21:38:21 -0500

    test cases for numbers based on 10

--- roman_numeral_kata/roman_numeral_test.go
+++ roman_numeral_kata/roman_numeral_test.go
@@ -16,6 +16,12 @@ func TestRomanNumerals(t *testing.T) {
 		{"5 gets converted to V", 6, "VI"},
 		{"5 gets converted to V", 7, "VII"},
 		{"5 gets converted to V", 8, "VIII"},
+		{"5 gets converted to V", 9, "IX"},
+		{"10 gets converted to X", 10, "X"},
+		{"14 gets converted to XIV", 14, "XIV"},
+		{"18 gets converted to XVIII", 18, "XVIII"},
+		{"20 gets converted to XX", 20, "XX"},
+		{"39 gets converted to XXXIX", 39, "XXXIX"},
 	}
 
 	for _, test := range cases {
